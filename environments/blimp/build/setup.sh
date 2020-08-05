d6a3ccd4012dff97204fd58e1d40e2aaec4fa1f0
Kevin Lin <kevin@kelda.io>
2020-08-05 09:22:27 -0700

    Custom env test

--- environments/blimp/build/setup.sh
+++ environments/blimp/build/setup.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-
-set -e
-
-apt update && apt install -y curl ca-certificates git
-curl -Lo blimp https://blimp-releases.s3-us-west-1.amazonaws.com/blimp-linux-0.13.21 && chmod +x blimp && mv blimp /usr/local/bin
-mkdir -p ~/.blimp
-git clone https://github.com/kelda/node-todo
-cd node-todo
+#!/bin/sh
+true
